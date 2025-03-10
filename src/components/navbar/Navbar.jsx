732bb85e0fddf052a93a6ec9f6a99e76a197b9d2
Ksenia Agalakova <agalaksu111@gmail.com>
2025-03-10 12:07:36 +0300

    Added GitHub link to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,12 +6,12 @@ import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlin
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
 import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
-import { DarkModeContext } from "../../context/darkModeContext";
-import { useContext } from "react";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import { useSidebar } from "../../context/SidebarContext";
+import { Link } from "@mui/material";
 
 const Navbar = () => {
-  const { dispatch } = useContext(DarkModeContext);
   const { toggleSidebar } = useSidebar();
 
   return (
@@ -25,12 +25,13 @@ const Navbar = () => {
       </div>
       <div className="wrapper">
         <div className="items">
+          <Link href="https://github.com/lembas-cracker/Admin-panel-for-React-Booking-App" className="item">
+            <GitHubIcon className="icon" />
+          </Link>
+
           <div className="item">
             <LanguageOutlinedIcon className="icon" />
             English
-          </div>
-          <div className="item">
-            <DarkModeOutlinedIcon className="icon" onClick={() => dispatch({ type: "TOGGLE" })} />
           </div>
           <div className="item">
             <FullscreenExitOutlinedIcon className="icon" />
@@ -44,11 +45,7 @@ const Navbar = () => {
             <div className="counter">2</div>
           </div>
           <div className="item">
-            <img
-              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-              alt=""
-              className="avatar"
-            />
+            <PersonOutlineIcon className="icon avatar" />
           </div>
         </div>
       </div>
